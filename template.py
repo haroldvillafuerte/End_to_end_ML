481bb036bcf74fdfd7faf2de48ca89d40442694b
haroldvillafuerte <haroldvill@outlook.com>
2024-10-16 18:29:58 +0200

    Requirements

--- template.py
+++ template.py
@@ -24,7 +24,7 @@ list_of_files = [
     "main.py",
     "app.py",
     "Dockerfile",
-    "requeriments.txt",
+    "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html"
