459e598d69544200fb0877abea7ad765d6e15723
haroldvillafuerte <haroldvill@outlook.com>
2024-10-21 14:49:31 +0200

    data validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 # logger.info("Welcom to our custom logging")
 
@@ -10,6 +11,18 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\n------------x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data validation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx------------x")
 except Exception as e:
     logger.exception(e)
     raise e
